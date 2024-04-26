e7f32885139ccb2835e225dd4e56834dcc7d3d6f
Boniface Kibata <bonikibata67@gmail.com>
2024-04-26 13:00:39 +0300

    files added

---
+++ book.js
@@ -0,0 +1,44 @@
+// Create a function that retruns the lenght of the array 
+
+let numberArray = [123,45,6,90,0,10000]
+
+// numberArray.length;
+
+// console.log(numberArray.length);
+
+// create a function that returns the sum of numbers in an array
+
+// const sumArray = [3,3,5,5]
+//  let total = 0;
+//  for (let i=0; i < sumArray.length; i++) {
+//     total += sumArray[i];
+//  }
+
+//  console.log(total);
+
+// sumArray.forEach((el)=> total += el)
+
+// console.log(total);
+
+//Create a function that checks if the name Onesmus exists in a given array
+
+// let namesArray = ["Justus", "Godfrey", "Onesmus", "Boniface"]
+
+// console.log(namesArray.includes("Onesmus"));
+
+//Create a function that return even numbers in a given array.
+
+// let intArrays = [1,2,3,4,4,55,6,7,8]
+
+// let even = [];
+// intArrays.forEach(element => {
+//     if( element%2 == 0 )
+//     even.push(element);
+// }); 
+
+// console.log(`Even numbers in an array are: ${even}`);
+
+
+
+
+
